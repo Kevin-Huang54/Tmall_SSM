57d76811b53578786bf97c686005ebe37f4c4320
Kevin Huang <silly_child@163.com>
2018-10-26 15:40:32 +0800

    用户的controller，只提供list功能，其他都不需要

---
+++ main/java/controller/UserController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import pojo.User;
+import service.UserService;
+import util.ImageUtil;
+import util.Page;
+import util.UploadedImageFile;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@RequestMapping("")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("admin_user_list")
+    public String listUser(Model model, Page page) {
+        PageHelper.offsetPage(page.getStart(), page.getCount());
+        List<User> us = userService.list();
+        int total = (int) new PageInfo<>(us).getTotal();
+        page.setTotal(total);
+        model.addAttribute("us", us);
+        model.addAttribute("page", page);
+        return "admin/listUser";
+    }
+}
